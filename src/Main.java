8bff3a4b7980cb5124838594fd1639cd3feb696b
HelenWeiss <elena.kherson@gmail.com>
2026-07-13 11:53:54 +0300

    Classes and Objects in Java: Practice. - Create an Employee class; - Attributes: name, age, salary, location; - Behavior raiseSalary; - Get creative! - Static or non-Static? - Create a Main class for building Employee instances (Objects); Build 2 Employee instances (Objects); - Give 1 Employee a raise; - Display the salaries of both Employees.

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@ import java.awt.Color;
 public class Main {
 
     public static void main (String [] args) {
+
+        // Practice: class Tree, enum TreeType
+/*
         Tree myFavoriteOakTreee = new Tree(25,5, TreeType.OAK);
         myFavoriteOakTreee.grow();
         System.out.println(myFavoriteOakTreee.treeType);
 
-        /*
+
         Tree myFavoriteOakTree = new Tree(120,12, TreeType.OAK);
         Tree myFavoriteMapleTree = new Tree(90,30, TreeType.MAPLE);
 
@@ -18,8 +21,8 @@ public class Main {
         if (myFavoriteMapleTree.heightFt > 100) {
             System.out.println("That's a tall " + myFavoriteMapleTree.treeType + " tree!");
         }
-*/
-        // optimized code upper with messaging "That's a tall tree" (class Main, rows 8-19)
+
+        // optimized code upper with messaging "That's a tall tree" (class Main, rows 12-23)
         // via method announceTallTree in a class Tree (rows 21-24)
         Tree myFavoriteOakTree = new Tree(120,12, TreeType.OAK);
         Tree myFavoriteMapleTree = new Tree(90,30, TreeType.MAPLE);
@@ -36,6 +39,21 @@ public class Main {
         Color myDefaultBlue  = Color.BLUE;
 
         Color brightBlue = myDefaultBlue.brighter();
+*/
+        // Practice, class Employee
+
+        // would be for creating Objects without constructor in class Employee
+        //Employee employeeOne = new Employee();
+        //Employee employeeTwo = new Employee();
+
+        // for creating Objects with constructor in class Employee
+        Employee employeeOne = new Employee("Matthias", 46, 5800, "Eschwege");
+        Employee employeeTwo = new Employee("Mark", 49, 8900, "Altenstadt");
+
+        employeeOne.raiseSalary();
+
+        System.out.println("A salary of " + employeeOne.name + " is " + employeeOne.salary);
+        System.out.println("A salary of " + employeeTwo.name + " is " + employeeTwo.salary);
 
     }
 }
