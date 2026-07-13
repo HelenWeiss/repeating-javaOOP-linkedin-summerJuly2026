97293b8bf262651722dc1012da5362e10c4a7f9e
HelenWeiss <elena.kherson@gmail.com>
2026-07-13 13:10:10 +0300

    Encapsulaion in Java: - in class Tree: * protected static Color trunkColor = new Color(102, 51, 0); * "private" is added for double heightFt; double trunkDiameterInches; TreeType treeType; * "public" is added for methods/functions Tree, void grow, static void announceTree As a result in a class Main params heightFt; trunkDiameterInches; treeType; are red cause they are not accesible out of their class tree with "private" --//System.out.println(myFavoriteMapleTree.treeType); --//System.out.println(myFavoriteMapleTree.heightFT); --//System.out.println(myFavoriteMapleTree.trunkDiameterInches);

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,19 @@ public class Main {
 /*
         Tree myFavoriteOakTreee = new Tree(25,5, TreeType.OAK);
         myFavoriteOakTreee.grow();
-        System.out.println(myFavoriteOakTreee.treeType);
+        //System.out.println(myFavoriteOakTreee.treeType); // to fix (now var is red) make "public" or delete "private" for double heightFt; double trunkDiameterInches; TreeType treeType;
 
 
         Tree myFavoriteOakTree = new Tree(120,12, TreeType.OAK);
         Tree myFavoriteMapleTree = new Tree(90,30, TreeType.MAPLE);
 
-        if (myFavoriteOakTree.heightFt > 100) {
-            System.out.println("That's a tall " + myFavoriteOakTree.treeType + " tree!");
-        }
+        //if (myFavoriteOakTree.heightFt > 100) { // to fix (now var is red) make "public" or delete "private" for double heightFt; double trunkDiameterInches; TreeType treeType;
+        //    System.out.println("That's a tall " + myFavoriteOakTree.treeType + " tree!"); // to fix (now var is red) make "public" or delete "private" for double heightFt; double trunkDiameterInches; TreeType treeType;
+        //}
 
-        if (myFavoriteMapleTree.heightFt > 100) {
-            System.out.println("That's a tall " + myFavoriteMapleTree.treeType + " tree!");
-        }
+        //if (myFavoriteMapleTree.heightFt > 100) { // to fix (now var is red) make "public" or delete "private" for double heightFt; double trunkDiameterInches; Tree
+        //    System.out.println("That's a tall " + myFavoriteMapleTree.treeType + " tree!"); // to fix (now var is red) make "public" or delete "private" for double heightFt; double trunkDiameterInches; TreeType treeType;
+        //}
 
         // optimized code upper with messaging "That's a tall tree" (class Main, rows 12-23)
         // via method announceTallTree in a class Tree (rows 21-24)
@@ -39,9 +39,13 @@ public class Main {
         Color myDefaultBlue  = Color.BLUE;
 
         Color brightBlue = myDefaultBlue.brighter();
+
+        //System.out.println(myFavoriteMapleTree.treeType); // to fix (now var is red) make "public" or delete "private" for double heightFt; double trunkDiameterInches; TreeType treeType;
+        //System.out.println(myFavoriteMapleTree.heightFT); // to fix (now var is red) make "public" or delete "private" for double heightFt; double trunkDiameterInches; TreeType treeType;
+        //System.out.println(myFavoriteMapleTree.trunkDiameterInches); // to fix (now var is red) make "public" or delete "private" for double heightFt; double trunkDiameterInches; TreeType treeType;
 */
         // Practice, class Employee
-
+/*
         // would be for creating Objects without constructor in class Employee
         //Employee employeeOne = new Employee();
         //Employee employeeTwo = new Employee();
@@ -54,6 +58,7 @@ public class Main {
 
         System.out.println("A salary of " + employeeOne.name + " is " + employeeOne.salary);
         System.out.println("A salary of " + employeeTwo.name + " is " + employeeTwo.salary);
+*/
 
     }
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,27 +1,29 @@
 import java.awt.*; // for static Color attribute
 
 public class Tree {
+
+    protected static Color trunkColor = new Color(102, 51, 0);
     // attributes of the class
-    double heightFt;
-    double trunkDiameterInches;
-    TreeType treeType;
+    private double heightFt;
+    private double trunkDiameterInches;
+    private TreeType treeType;
     static Color TRUNK_COLOR = new Color(102, 51, 0); // static Color attribute
 
     // constructor
-    Tree(double heightFt, double trunkDiameterInches, TreeType treeType) {
+    public Tree(double heightFt, double trunkDiameterInches, TreeType treeType) {
         this.heightFt = heightFt;
         this.trunkDiameterInches = trunkDiameterInches;
         this.treeType = treeType;
     }
 
     // behavior (method)
-    void grow() {
+    public void grow() {
         this.heightFt = this.heightFt + 10;
         this.trunkDiameterInches = this.trunkDiameterInches + 1;
     }
 
     // behavior (method)
-    static void announceTree() {              // static method
+    public static void announceTree() {              // static method
         System.out.println("Look out for that " + TRUNK_COLOR + " tree!");
     }
 
